3524576154bc56525633a7b192f63ab2c096cd99
andrewipark <andrewipark@users.noreply.github.com>
2016-11-09 21:25:28 -0600

    small code quality fixups

--- sCanandroid/app/src/main/java/com/example/scan/scan/homepage.java
+++ sCanandroid/app/src/main/java/com/example/scan/scan/homepage.java
@@ -21,6 +21,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import android.content.Context;
 import android.util.SparseArray;
@@ -35,15 +37,16 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 public class homepage extends AppCompatActivity {
+    private static final int
+            REQUEST_IMAGE_CAPTURE = 1, PICK_IMAGE = 100;
+    private static final String LOG_TAG = "testOCR.java";
+    private static final String TAG = "testOCR.java";
+
+    public String textFileName = "";
+
     Button btnscan;
     Button btnPicLib;
     Button btnDocList;
-    public String textFileName = "";
-    private static final int
-        REQUEST_IMAGE_CAPTURE = 1, PICK_IMAGE = 100;
-
-    private static final String LOG_TAG = "testOCR.java";
-    private static final String TAG = "testOCR.java";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,21 +57,20 @@ public class homepage extends AppCompatActivity {
         s = s.trim();
 
         if (s.contains(" "))
-            s = s.substring(0,s.indexOf(" ")); // trim to first name
+            s = s.substring(0, s.indexOf(" ")); // trim to first name
 
-
-        s = s.substring(0,1).toUpperCase() + s.substring(1,s.length());
+        s = s.substring(0, 1).toUpperCase() + s.substring(1, s.length());
         TextView user = (TextView) findViewById(R.id.showuser);
-        user.setText("Hello " + s + "!");
+        user.setText("Hello, " + s + "!");
 
         // Scan button invokes Camera
         btnscan = (Button) findViewById(R.id.scanbutton);
         btnscan.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
 
                 }
             }
@@ -93,7 +95,7 @@ public class homepage extends AppCompatActivity {
         });
     }
 
-    private void openGallery(){
+    private void openGallery() {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
     }
@@ -102,7 +104,8 @@ public class homepage extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode != RESULT_OK) return; // this should never happen
+        if (BuildConfig.DEBUG && resultCode != RESULT_OK)
+            throw new RuntimeException("Result NOT ok for some reason");
 
         Bitmap imageBitmap = null;
 
@@ -113,7 +116,7 @@ public class homepage extends AppCompatActivity {
         }
 
         // IF PICTURE IS TAKEN FROM PHOTO LIBRARY
-        if (requestCode == PICK_IMAGE){
+        if (requestCode == PICK_IMAGE) {
             Uri imageUri = data.getData();
 
             try {
@@ -124,53 +127,51 @@ public class homepage extends AppCompatActivity {
             }
         }
 
-        assert imageBitmap != null; // this should never happen
-
+        if (BuildConfig.DEBUG && imageBitmap == null)
+            throw new RuntimeException("how is this null?");
 
         // PREPROCESSING (has been removed temporarily due to dysfunctionality.
-        //imageBitmap = PreProcessing.doStuff(imageBitmap);
+        // imageBitmap = PreProcessing.doStuff(imageBitmap);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Type text file name:");
 
-// Set up the input
+        // Set up the input
         final EditText input = new EditText(this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
         builder.setView(input);
 
-// Set up the buttons
+        // Set up the buttons
         final Bitmap finalImageBitmap = imageBitmap;
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                textFileName = input.getText().toString();
+                // OCR stuff
+                // move this outside if you uncomment the textview
                 String answer = "";
                 Context context = getApplicationContext();
                 TextRecognizer ocrFrame = new TextRecognizer.Builder(context).build();
                 Frame frame = new Frame.Builder().setBitmap(finalImageBitmap).build();
                 SparseArray<TextBlock> textBlocks = ocrFrame.detect(frame);
-                for (int i = 0; i < textBlocks.size(); i++) {
+                for (int i = 0; i < textBlocks.size(); i++) // probably should use stringbuilder
                     answer += textBlocks.get(textBlocks.keyAt(i)).getValue();
-                }
 
+                // save plz
+                textFileName = input.getText().toString();
+                if (textFileName.equals("")) {
+                    // random values
+                    textFileName = "sCan-" + (int) (Math.random() * 100000);
+                }
 
                 try {
-
-                    OutputStreamWriter out= new OutputStreamWriter(openFileOutput(textFileName + ".txt", 0));
-
+                    OutputStreamWriter out = new OutputStreamWriter(openFileOutput(textFileName + ".txt", 0));
                     out.write(answer);
-
                     out.close();
-
-                    Toast.makeText(getApplicationContext(), "The contents are saved in the file "+ textFileName + ".txt.", Toast.LENGTH_LONG).show();
-
-                }
-
-                catch (Throwable t) {
-
-                    Toast.makeText(getApplicationContext(), "Exception: "+t.toString(), Toast.LENGTH_LONG).show();
-
+                    Toast.makeText(getApplicationContext(), "Saved text to " + textFileName + ".txt", Toast.LENGTH_SHORT).show();
+                } catch (Throwable t) {
+                    Toast.makeText(getApplicationContext(), "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+                    if (BuildConfig.DEBUG)
+                        t.printStackTrace();
                 }
             }
         });
@@ -185,15 +186,12 @@ public class homepage extends AppCompatActivity {
         builder.show();
 
         // OCR
-
-
-        //passing the detected text to DisplayText.class
-        /*Intent intent = new Intent(this, DisplayText.class);
+        // uncomment this code to just show the text without saving it
+        /*if (false) {Intent intent = new Intent(this, DisplayText.class);
         intent.putExtra("text", answer);
-        startActivity(intent);*/
+        startActivity(intent);}*/
 
-
-        // THIS IS USED FOR DEBUGGING BITMAPS. TRIAL PURPOSE ONLY
+        // uncomment this code to just show what was captured
         /*ByteArrayOutputStream stream = new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
         byte[] byteArray = stream.toByteArray();
